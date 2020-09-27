4de253e08e0a788b220421e89168d354e6ed1cdc
luuthanhvan <thanhvanluu.3199@gmail.com>
2020-09-27 19:00:23 +0700

    Image processing fundamental: Histogram stretch and Segmentation

--- src/ImageProcessing/mainwindow.cpp
+++ src/ImageProcessing/mainwindow.cpp
@@ -1,12 +1,15 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include <QFileDialog>
+#include <QFileInfo>
 #include <QPixmap>
 #include <QLabel>
 #include <QString>
 #include <QDebug>
 #include <QImage>
 #include <QRgb>
+#include <QColor>
+#include <QDir>
 
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow){
     ui->setupUi(this);
@@ -42,6 +45,7 @@ void MainWindow::on_btn_browse_clicked(){
     //label->show();
 }
 
+/* Histogram slide method => increasing or decreasing BRIGHTNESS of a gray image */
 void MainWindow::on_btn_hisSlide_gray_clicked(){
     QString fileName = ui->ln_fileName->text(); // get file name from ln_fileName (QLineEdit)
     qDebug() << "File name: " << fileName; // test
@@ -54,9 +58,9 @@ void MainWindow::on_btn_hisSlide_gray_clicked(){
     // travesing all pixels of imageIn
     for(int x = 0; x < imageIn.width(); x++){
         for(int y = 0; y < imageIn.height(); y++){
-            QRgb color = imageIn.pixel(x, y); // get pixel value at (x,y)
-            int grayIn = qGray(color); // convert color value at (x,y) to gray level
-            int grayOut = grayIn + c; // change gray level aming to increasing or decreasing brightness
+            QRgb colorIn = imageIn.pixel(x, y); // get pixel value at (x,y)
+            int grayIn = qGray(colorIn); // convert color value at (x,y) to gray value
+            int grayOut = grayIn + c; // change gray value aming to increasing or decreasing brightness
 
             // check threshold
             if(grayOut > 255)
@@ -65,14 +69,16 @@ void MainWindow::on_btn_hisSlide_gray_clicked(){
                 if(grayOut < 0)
                    grayOut = 0;
 
-            QRgb colorOut = qRgb(grayOut, grayOut, grayOut); // gray image
+            QRgb colorOut = qRgb(grayOut, grayOut, grayOut); // color out for image out
             imageOut.setPixel(x, y, colorOut);
         }
     }
+    // display image input and image output
     displayImage(imageIn, QFileInfo(fileName).fileName());
     displayImage(imageOut, QFileInfo(fileName).fileName()+"_his_slide");
 }
 
+/* Histogram slide method => increasing or decreasing BRIGHTNESS of a color image */
 void MainWindow::on_btn_hisSlide_Color_clicked(){
     QString fileName = ui->ln_fileName->text(); // get file name from ln_fileName (QLineEdit)
     qDebug() << "File name: " << fileName; // test
@@ -98,10 +104,125 @@ void MainWindow::on_btn_hisSlide_Color_clicked(){
                 if(vOut < 0)
                    vOut = 0;
 
-            QColor colorOut = QColor::fromHsv(h, s, vOut); // color image
+            QColor colorOut = QColor::fromHsv(h, s, vOut); // color out for image out
             imageOut.setPixel(x, y, colorOut.rgb());
         }
     }
+    // display image input and image output
     displayImage(imageIn, QFileInfo(fileName).fileName());
     displayImage(imageOut, QFileInfo(fileName).fileName()+"_his_slide");
+}
+
+/* Histogram stretch method => increasing or decreasing CONTRAST of a gray image */
+void MainWindow::on_btn_hisStretch_gray_clicked(){
+    QString fileName = ui->ln_fileName->text(); // get file name from ln_fileName (QLineEdit)
+    qDebug() << "File name: " << fileName; // test
+
+    QImage imageIn(fileName); // read image from file name
+    QImage imageOut(imageIn.width(), imageIn.height(), QImage::Format_ARGB32);
+
+    int c = ui->lb_c->text().toFloat(); // convert string to float number
+
+    // travesing all pixels of imageIn
+    for(int x = 0; x < imageIn.width(); x++){
+        for(int y = 0; y < imageIn.height(); y++){
+            QRgb colorIn = imageIn.pixel(x, y); // get pixel value at (x,y)
+            int grayIn = qGray(colorIn); // convert color value at (x,y) to gray value
+            int grayOut = grayIn * c; // change gray value to increasing or decreasing the contrast of the image
+
+            // check threshold
+            if(grayOut > 255)
+                grayOut = 255;
+            else
+                if(grayOut < 0)
+                    grayOut = 0;
+            QRgb colorOut = qRgb(grayOut, grayOut, grayOut); // color out for image out
+            imageOut.setPixel(x, y, colorOut);
+        }
+    }
+    // save image out
+    /*QString selectedFilter;
+    QString name = QFileDialog::getSaveFileName(this, "Save image", QDir::currentPath(),
+                                                "BMP (*.bmp);;PNG (*.png);;JPEG (*.jpg);;All files (*.*)",
+                                                &selectedFilter);
+
+    if(selectedFilter == "BMP (*.bmp)")
+        imageOut.save(name, "BMP");
+    else
+        if(selectedFilter == "PNG (*.png)")
+            imageOut.save(name, "PNG");
+        else
+            if(selectedFilter == "JPEG (*.jpg)")
+                imageOut.save(name, "JPEG");
+            else
+                imageOut.save(name);
+    */
+    // display image input and image output
+    displayImage(imageIn, QFileInfo(fileName).fileName());
+    displayImage(imageIn, QFileInfo(fileName).fileName()+"_his_stretch");
+}
+
+/* Histogram stretch method => increasing or decreasing CONTRAST of a color image */
+void MainWindow::on_btn_hisStretch_Color_clicked(){
+    QString fileName = ui->ln_fileName->text(); // get file name from ln_fileName (QLineEdit)
+    qDebug() << "File name: " << fileName; // test
+
+    QImage imageIn(fileName);
+    QImage imageOut(imageIn.width(), imageIn.height(), QImage::Format_ARGB32);
+
+    int c = ui->ln_c->text().toFloat(); // convert string to float number
+
+    // travesing all pixels of imageIn
+    for(int x = 0; x < imageIn.width(); x++){
+        for(int y = 0; y < imageIn.height(); y++){
+            QColor colorIn = imageIn.pixel(x, y);
+            int h, s, v;
+            colorIn.getHsv(&h, &s, &v);
+
+            int vOut = v * c;
+
+            // check threshold
+            if(vOut > 255)
+                vOut = 255;
+            else
+                if(vOut < 0)
+                    vOut = 0;
+
+            QColor colorOut = QColor::fromHsv(h, s, vOut); // color out for image out
+            imageOut.setPixel(x, y, colorOut.rgb());
+        }
+    }
+    // display image input and image output
+    displayImage(imageIn, QFileInfo(fileName).fileName());
+    displayImage(imageIn, QFileInfo(fileName).fileName()+"_his_stretch");
+}
+
+void MainWindow::on_btn_segmentation_gray_clicked(){
+    QString fileName = ui->ln_fileName->text(); // get file name from ln_fileName (QLineEdit)
+    qDebug() << "File name: " << fileName; // test
+
+    QImage imageIn(fileName);
+    QImage imageOut(imageIn.width(), imageIn.height(), QImage::Format_ARGB32);
+
+    int c = ui->ln_c->text().toInt(); // convert string to number
+
+    // travesing all pixels of imageIn
+    for(int x = 0; x < imageIn.width(); x++){
+        for(int y = 0; y < imageIn.height(); y++){
+            QRgb colorIn = imageIn.pixel(x, y); // get pixel value at (x,y)
+            int gray = qGray(colorIn); // convert color value at (x,y) to gray value
+
+            // check threshold
+            if(gray > c)
+                gray = 255;
+            else
+                gray = 0;
+
+            QRgb colorOut = qRgb(gray, gray, gray);
+            imageOut.setPixel(x, y, colorOut);
+        }
+    }
+    // display image input and image output
+    displayImage(imageIn, QFileInfo(fileName).fileName());
+    displayImage(imageIn, QFileInfo(fileName).fileName()+"_segmentation");
 }
--- src/ImageProcessing/mainwindow.h
+++ src/ImageProcessing/mainwindow.h
@@ -14,12 +14,20 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
     void displayImage(QImage &img, QString title);
+//    void saveImage(QImage &imgOut, QString fileName);
 
 private slots:
     void on_btn_browse_clicked();
+
     void on_btn_hisSlide_gray_clicked();
 
     void on_btn_hisSlide_Color_clicked();
+
+    void on_btn_hisStretch_gray_clicked();
+
+    void on_btn_hisStretch_Color_clicked();
+
+    void on_btn_segmentation_gray_clicked();
 
 private:
     Ui::MainWindow *ui;
